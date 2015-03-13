eef4e83b0f822639bc5e9ab4bf8667efdf982ed8
wilzh40 <wilzh40@gmail.com>
2015-03-12 23:18:17 -0700

    Initial Commit

---
+++ WormBase.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import lxml
+
+import json 
+import requests
+import grequests
+import sys 
+import re
+
+
+# Variables
+# A couple of test genes
+genes = ["WBGene00001187", "WBGene00000529", "WBGene00004830", "WBGene00004831", "WBGene00006772", "WBGene00022106", "WBGene00022240", "WBGene00006318"]
+geneIDs = []
+
+
+# Gene class
+
+class Gene:
+    def __init__(self,*args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+# Connect with the WormBase API
+
+baseURL = "http://api.wormbase.org/rest/widget/gene/"
+headers = {'content-type': 'application/json'}
+r = requests.get(baseURL + "WBGene00001187/description", headers=headers) 
+print(r.json())
+
+
+def getGene(wbid):
+    r = requests.get(baseURL + wbid + "/overview", headers=headers) 
+    
+    j = r.json()
+    geneID = wbid 
+    proteinName = j['fields']['name']['data']['label']
+    description = j['fields']['concise_description']['data']['text']
+    print description
+    geneClass = ""
+    gene = Gene(geneID, proteinName, description,  geneClass) 
+    return gene 
+
+print getGene('WBGene00001187')
+
+# Create the columns based on the Excel Spreadsheet
+
+columns = ["Gene WB ID","Protein ShortName", "Gene Public Name", "Description", "PMID for evidence", "Protein Name", "Gene Class", "Expression Pattern in Adults", "PMID for expression evidence", "Expressed in neurons", "Expression Info" ]
+
+# Create rows based on Protein IDs
+# Save to file
+
